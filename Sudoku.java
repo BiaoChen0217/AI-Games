7561377cfc5c210be09c5a38571d0cbb97fded1f
BiaoChen0217 <60233455+BiaoChen0217@users.noreply.github.com>
2020-02-24 23:31:52 -0500

    Update Sudoku.java

--- Sudoku.java
+++ Sudoku.java
@@ -6,19 +6,25 @@ import java.util.Scanner;
 
 /**
  *
+ *@param path 
+ *
+ *
+ *description:read the file path and write the puzzle to the array
  */
-public class Sudoku {
+public class Sudoku extends AIGame{
     private int[][] matrix;
-    static String path="s03a.txt" ;  //!!!!←change that
-    
+    static String path;
+	private static Scanner input;
     
     public Sudoku(int[][] matrix) {
+  	
         this.matrix = matrix;
     }
  
     
     
 	public Sudoku(String path) throws FileNotFoundException {
+		
 		readPuzzle(path);
 		
 	}
@@ -33,7 +39,9 @@ public class Sudoku {
 	 */
     
 	public static int[][] readPuzzle(String path) throws FileNotFoundException{
-		
+		input = new Scanner(System.in);
+    	System.out.println("Please input the puzzle path!Example:C:\\Users\\30133\\Desktop\\s01a.txt");
+    	path=input.nextLine();
 		Scanner sc = new Scanner(new File(path)); 
 		int[][] grid = new int[9][9];
 		
@@ -58,16 +66,16 @@ public class Sudoku {
         
         Sudoku s = new Sudoku(readPuzzle(path));
         
-        s.backTrace(0, 0);
+        s.solve(0, 0);
     }
  
     /**
      * Sudoku
-     *Using backtrace
+     *Using backtrack
      * @param i row
      * @param j col
      */
-    private void backTrace(int i, int j) {
+    private void solve(int i, int j) {
         if (i == 8 && j == 9) {
             //if the array is finished,game over
             System.out.println("Game Over!");
@@ -91,7 +99,7 @@ public class Sudoku {
                 if (check(i, j, k)) {
                     
                     matrix[i][j] = k;
-                    backTrace(i, j + 1);
+                    solve(i, j + 1);
                     
                     //if the code can't continue,we need to change the first number to 0.
                     matrix[i][j] = 0;
@@ -99,7 +107,7 @@ public class Sudoku {
             }
         } else {
            
-            backTrace(i, j + 1);
+        	solve(i, j + 1);
         }
     }
  
@@ -108,19 +116,72 @@ public class Sudoku {
      * @param row    
      * @param line   
      * @param number 
-     * @return
+     * 
+     * function:cheak very row, line, sub-grid
+     * 
+     * 
+     * 
+     * @return 
      */
     private boolean check(int row, int line, int number) {
        
-    	//make sure that there are not two same number
-        for (int i = 0; i < 9; i++) {
-            if (matrix[row][i] == number || matrix[i][line] == number) {
+    	
+        if(!isRowValid(row,number)||!isColumnValid(line,number)||!isSubGridValid(row,line,number))
+        {
+        	return false;
+        }
+       
+        return true;
+    }
+ /**
+  * 
+  * 
+  * @param row 
+  * @param number
+  * 
+  * description:check very row to confirm there are not two same number
+  * 
+  */
+    private boolean isRowValid(int row ,int number) {
+    	for (int i = 0; i < 9; i++) {
+            if (matrix[row][i] == number ) {
                 return false;
             }
         }
-
-        //check the sub-grid 
-        int tempRow = row / 3;
+		return true;
+    }
+    
+    
+    /**
+     * 
+     * 
+     * @param line 
+     * @param number
+     * 
+     * description:check very line to confirm there are not two same number
+     * 
+     */
+    
+    private boolean isColumnValid(int line ,int number) {
+    	for (int i = 0; i < 9; i++) {
+            if (matrix[i][line] == number ) {
+                return false;
+            }
+        }
+		return true;
+    }
+    
+    /**
+     * 
+     * @param row
+     * @param line 
+     * @param number
+     * 
+     * description:check very sub-grid to confirm there are not two same number
+     * 
+     */
+    private boolean isSubGridValid(int row,int line ,int number) {
+    	int tempRow = row / 3;
         int tempLine = line / 3;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -129,14 +190,23 @@ public class Sudoku {
                 }
             }
         }
- 
         return true;
     }
- 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**
-     * print
+     * print the whole array,if true
+     * @return if the problem can be solved,return true to AI Game.
      */
-    public void printArray() {
+    public boolean printArray() {
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 System.out.print(matrix[i][j] + " ");
@@ -144,6 +214,7 @@ public class Sudoku {
             System.out.println();
         }
         System.out.println();
+        return true;
     }
 
 }
